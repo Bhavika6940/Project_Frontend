0d61467cc33c8c8e1f642c6d2c74d94278a6c45b
Bhavika6940 <115462254+Bhavika6940@users.noreply.github.com>
2025-05-29 15:32:05 +0530

    Update AssessmentUpload.js

--- src/pages/AssessmentUpload.js
+++ src/pages/AssessmentUpload.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 import api from "../service/api";
 
 export default function UploadQuestions() {
